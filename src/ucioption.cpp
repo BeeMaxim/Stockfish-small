1738c55665834f1c5cf296f3df6ea8a3cdcffab7
BeeMaxim <bekoev.maxim@yandex.ru>
2025-02-04 23:44:52 +0300

    hashtable

--- src/ucioption.cpp
+++ src/ucioption.cpp
@@ -56,29 +56,30 @@ bool CaseInsensitiveLess::operator() (const string& s1, const string& s2) const 
 
 void init(OptionsMap& o) {
 
-  constexpr int MaxHashMB = Is64Bit ? 33554432 : 2048;
+  // constexpr int MaxHashMB = Is64Bit ? 33554432 : 2048;
+  constexpr int MaxHashMB = 1024 * 1024;
 
   o["Debug Log File"]        << Option("", on_logger);
   o["Contempt"]              << Option(24, -100, 100);
   o["Analysis Contempt"]     << Option("Both var Off var White var Black var Both", "Both");
   o["Threads"]               << Option(1, 1, 512, on_threads);
-  o["Hash"]                  << Option(16, 1, MaxHashMB, on_hash_size);
+  o["Hash"]                  << Option(1, 1, MaxHashMB, on_hash_size); // 16, 1
   o["Clear Hash"]            << Option(on_clear_hash);
   o["Ponder"]                << Option(false);
   o["MultiPV"]               << Option(1, 1, 500);
   o["Skill Level"]           << Option(20, 0, 20);
   o["Move Overhead"]         << Option(10, 0, 5000);
-  o["Slow Mover"]            << Option(100, 10, 1000);
+//  o["Slow Mover"]            << Option(100, 10, 1000);
   o["nodestime"]             << Option(0, 0, 10000);
-  o["UCI_Chess960"]          << Option(false);
-  o["UCI_AnalyseMode"]       << Option(false);
+//  o["UCI_Chess960"]          << Option(false);
+//  o["UCI_AnalyseMode"]       << Option(false);
   o["UCI_LimitStrength"]     << Option(false);
-  o["UCI_Elo"]               << Option(1350, 1350, 2850);
+//  o["UCI_Elo"]               << Option(1350, 1350, 2850);
   o["UCI_ShowWDL"]           << Option(false);
 //  o["SyzygyPath"]            << Option("<empty>", on_tb_path);
-  o["SyzygyProbeDepth"]      << Option(1, 1, 100);
-  o["Syzygy50MoveRule"]      << Option(true);
-  o["SyzygyProbeLimit"]      << Option(7, 0, 7);
+//  o["SyzygyProbeDepth"]      << Option(1, 1, 100);
+//  o["Syzygy50MoveRule"]      << Option(true);
+ // o["SyzygyProbeLimit"]      << Option(7, 0, 7);
 }
 
 
