8c1617562dc4456559bb67c838a2c73530886f6a
BeeMaxim <bekoev.maxim@yandex.ru>
2025-02-04 23:19:36 +0300

    next

--- src/uci.cpp
+++ src/uci.cpp
@@ -242,18 +242,19 @@ void UCI::loop(int argc, char* argv[]) {
       // So 'ponderhit' will be sent if we were told to ponder on the same move the
       // user has played. We should continue searching but switch from pondering to
       // normal search.
+      /*
       else if (token == "ponderhit")
           Threads.main()->ponder = false; // Switch to normal search
 
       else if (token == "uci")
           sync_cout << "id name " << engine_info(true)
                     << "\n"       << Options
-                    << "\nuciok"  << sync_endl;
+                    << "\nuciok"  << sync_endl;*/
 
-      else if (token == "setoption")  setoption(is);
+      // else if (token == "setoption")  setoption(is);
       else if (token == "go")         go(pos, is, states);
       else if (token == "position")   position(pos, is, states);
-      else if (token == "ucinewgame") Search::clear();
+      // else if (token == "ucinewgame") Search::clear();
       else if (token == "isready")    sync_cout << "readyok" << sync_endl;
 
       // Additional custom non-UCI commands, mainly for debugging.
