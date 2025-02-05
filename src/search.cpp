efd709716c064181c062748e92ac5ff5c9089b63
BeeMaxim <bekoev.maxim@yandex.ru>
2025-02-05 17:42:23 +0300

    test

--- src/search.cpp
+++ src/search.cpp
@@ -1670,7 +1670,7 @@ moves_loop: // When in check, search starts from here
 
   void update_all_stats(const Position& pos, Stack* ss, Move bestMove, Value bestValue, Value beta, Square prevSq,
                         Move* quietsSearched, int quietCount, Move* capturesSearched, int captureCount, Depth depth) {
-
+    /*
     int bonus1, bonus2;
     Color us = pos.side_to_move();
     Thread* thisThread = pos.this_thread();
@@ -1707,7 +1707,7 @@ moves_loop: // When in check, search starts from here
         moved_piece = pos.moved_piece(capturesSearched[i]);
         captured = type_of(pos.piece_on(to_sq(capturesSearched[i])));
         captureHistory[moved_piece][to_sq(capturesSearched[i])][captured] << -bonus1;
-    }
+    }*/
   }
 
 
