2b17753a1cba0a7e1b3eea3c83f198300ac05edb
BeeMaxim <bekoev.maxim@yandex.ru>
2025-02-05 16:50:02 +0300

    test

--- src/main.cpp
+++ src/main.cpp
@@ -44,8 +44,8 @@ int main(int argc, char* argv[]) {
   // Position::init();
   // Bitbases::init();
   // Endgames::init();
-  Threads.set(size_t(Options["Threads"]));
-  Search::clear(); // After threads are up
+  //Threads.set(size_t(Options["Threads"]));
+  //Search::clear(); // After threads are up
 
   UCI::loop(argc, argv);
 
